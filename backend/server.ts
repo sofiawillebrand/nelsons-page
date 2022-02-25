c4897b9c7410af2b34ae99bd5a273f84813518fa
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-02-25 09:39:28 +0100

    feat: add backend/frontend folders and setup backend

---
+++ backend/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/eco-friendly-api";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.Promise = Promise;
+
+// Defines the port the app will run on. Defaults to 8080, but can be
+// overridden when starting the server. For example:
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+// Our own middleware that checks if the database is connected before going forward to our endpoints
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "Service unavailable" });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`);
+});
